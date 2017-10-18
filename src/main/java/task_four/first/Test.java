b677f4020258c56e7e241926046688849af97e16
Eldar Ziatdinov <ziatdinov.eldar9@gmail.com>
2017-10-18 19:07:48 +0400

    Deleted imports in the Test

--- src/main/java/task_four/first/Test.java
+++ src/main/java/task_four/first/Test.java
@@ -1,11 +1,5 @@
 package task_four.first;
 
-import task_four.first.bean.DataBean;
-import task_four.first.bean.DataBeanTwo;
-import task_four.first.bean.TelephoneBean;
-import task_four.first.pojo.Data;
-import task_four.first.pojo.Telephone;
-
 public class Test {
 
     public void runTest() {
