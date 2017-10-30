8f06e254192773f92160d388ca1e199bac0fe552
Eldar Ziatdinov <ziatdinov.eldar9@gmail.com>
2017-10-30 20:17:04 +0400

    Added example parsers.

---
+++ src/main/java/task_eight/Main.java
@@ -0,0 +1,8 @@
+package task_eight;
+
+public class Main {
+
+    public static void main(String[] args) {
+        new Test().process();
+    }
+}
---
+++ src/main/java/task_eight/Test.java
@@ -0,0 +1,22 @@
+package task_eight;
+
+import org.apache.log4j.Logger;
+import task_eight.dom.DomParser;
+import task_eight.sax.SaxHandler;
+import task_eight.sax.SaxParser;
+import task_four.second.domain.Publisher;
+
+public class Test {
+    private static Logger logger = Logger.getLogger(Test.class);
+
+    public void process() {
+        String pathToXML = "src\\main\\resources\\thebestofpablisher.xml";
+        DomParser domParser = new DomParser();
+        SaxParser saxParser = new SaxParser();
+        Publisher publisherDom = domParser.parsePublisher(pathToXML);
+        Publisher publisherSax = saxParser.parsePublisher(pathToXML);
+        logger.info("DOM: " + publisherDom);
+        logger.info("SAX: " + publisherSax);
+    }
+
+}
