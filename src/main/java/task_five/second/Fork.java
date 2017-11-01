2f180e25994eeaaac0407aec835b9defea054b7d
Eldar Ziatdinov <ziatdinov.eldar9@gmail.com>
2017-11-02 01:24:41 +0400

    Deleted Fork, because this is markerclass - antipattern

--- src/main/java/task_five/second/Fork.java
+++
@@ -1,7 +0,0 @@
-package task_five.second;
-
-import java.util.concurrent.locks.ReentrantLock;
-
-//Маркерный класс - это антипаттерн. По возможности его нужно избегать
-public class Fork extends ReentrantLock {
-}
