0a5d214ec2a571716cb4e0965f1051d792d6c1d7
Eldar Ziatdinov <ziatdinov.eldar9@gmail.com>
2017-11-01 22:30:05 +0400

    Added validator xml by xsd. Renamed file xml

--- src/main/java/task_eight/Test.java
+++ src/main/java/task_eight/Test.java
@@ -9,7 +9,9 @@ public class Test {
     private static Logger logger = Logger.getLogger(Test.class);
 
     public void process() {
-        String pathToXML = "src\\main\\resources\\thebestofpablisher.xml";
+        String pathToXML = "src\\main\\resources\\thebestofpublisher.xml";
+        String pathToXSD = "src\\main\\resources\\thebestofpublisher.xsd";
+        ValidationXmlByXsd.validateXml(pathToXSD,pathToXML);
         DomParser domParser = new DomParser();
         SaxParser saxParser = new SaxParser();
         Publisher publisherDom = domParser.parsePublisher(pathToXML);
---
+++ src/main/java/task_eight/ValidationXmlByXsd.java
@@ -0,0 +1,36 @@
+package task_eight;
+
+import org.apache.log4j.Logger;
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class ValidationXmlByXsd {
+
+    private static Logger logger = Logger.getLogger(ValidationXmlByXsd.class);
+
+    public static void validateXml(String xsd_path, String xml_path) {
+        try {
+            File schemaFile = Paths.get(xsd_path).toFile();
+            File dataFile = Paths.get(xml_path).toFile();
+            SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            Schema schema = schemaFactory.newSchema(schemaFile);
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(new FileInputStream(dataFile)));
+            logger.info("XML valid by XSD!");
+        } catch (SAXException e) {
+            logger.error("SAX Error!", e);
+        } catch (IOException e) {
+            logger.error("IO Error", e);
+        }
+    }
+}
