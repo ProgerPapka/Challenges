817f06ac1840befcb572dd6fceee8afc7450bbd0
Eldar Ziatdinov <ziatdinov.eldar9@gmail.com>
2017-11-04 10:06:07 +0400

    Added test to serialize mock

---
+++ src/test/java/task_seven/serialization/TextSerializerTest.java
@@ -0,0 +1,40 @@
+package task_seven.serialization;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TextSerializerTest {
+    @Test
+    public void writeAuthor() throws Exception {
+    }
+
+    @Test
+    public void readAuthor() throws Exception {
+    }
+
+    @Test
+    public void writeBook() throws Exception {
+    }
+
+    @Test
+    public void readBook() throws Exception {
+    }
+
+    @Test
+    public void writePublisher1() throws Exception {
+    }
+
+    @Test
+    public void readPublisher1() throws Exception {
+    }
+
+    @Test
+    public void writePublisher() throws Exception {
+    }
+
+    @Test
+    public void readPublisher() throws Exception {
+    }
+
+}
